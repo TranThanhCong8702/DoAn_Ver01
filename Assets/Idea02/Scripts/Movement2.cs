2291f94ec32e37e6f926e1c7fc48ff9f402e686c
congtran <congtran872002@gmail.com>
2024-04-02 11:38:12 +0700

    limit the ragdoll running velocity

--- Assets/Idea02/Scripts/Movement2.cs
+++ Assets/Idea02/Scripts/Movement2.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Movement2 : MonoBehaviour
 {
-    public GameObject leftLeg;
-    public GameObject rightLeg;
-    Rigidbody2D leftLegRB;
-    Rigidbody2D rightLegRB;
+    [SerializeField] Rigidbody2D leftLegRB;
+    [SerializeField] Rigidbody2D rightLegRB;
 
     [SerializeField] Rigidbody2D leftLegRBlow;
     [SerializeField] Rigidbody2D rightLegRBlow;
     [SerializeField] Rigidbody2D Hip;
     Animator anim;
+
+    [SerializeField] float MaxSpeed;
+    [SerializeField] float MinSpeed;
+
+    [SerializeField] float accelRate;
+    [SerializeField] float deAccelRate;
 
     [SerializeField] float speed = 2f;
     [SerializeField] float jumpHeight = 2f;
@@ -20,16 +24,12 @@ public class Movement2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        leftLegRB = leftLeg.GetComponent<Rigidbody2D>();
-        rightLegRB = rightLeg.GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
-
     }
 
     // Update is called once per frame
     void Update()
     {
-
         if (Input.GetAxisRaw("Horizontal") != 0)
         {
             if (Input.GetAxis("Horizontal") < 0)
@@ -37,24 +37,26 @@ public class Movement2 : MonoBehaviour
                 anim.Play("WalkLeft");
                 StartCoroutine(MoveLeft(legWait));
                 Hip.velocity = new Vector2(Hip.velocity.x, 0);
+                if (Mathf.Abs(MaxSpeed) < Mathf.Abs(Hip.velocity.x))
+                {
+                    Hip.velocity = new Vector2(-MaxSpeed, Hip.velocity.y);
+                }
             }
             else
             {
                 anim.Play("WalkRight");
                 StartCoroutine(MoveRight(legWait));
                 Hip.velocity = new Vector2(Hip.velocity.x, 0);
+                if (Mathf.Abs(MaxSpeed) < Mathf.Abs(Hip.velocity.x))
+                {
+                    Hip.velocity = new Vector2(MaxSpeed, Hip.velocity.y);
+                }
             }
 
         }
         else
         {
             anim.Play("idle");
-            if(Mathf.Abs(Hip.velocity.x) < 3f)
-            {
-                leftLegRB.velocity = new Vector2(0, Hip.velocity.y);
-                rightLegRB.velocity = new Vector2(0, Hip.velocity.y);
-                Hip.velocity = new Vector2(0, Hip.velocity.y);
-            }
         }
         //if (Input.GetKeyDown(KeyCode.Space))
         //{
