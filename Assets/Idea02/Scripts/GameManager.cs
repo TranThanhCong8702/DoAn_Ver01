f4914b935c5a28300f6963717a60ff1227f7658d
congtran <congtran872002@gmail.com>
2024-04-17 15:21:55 +0700

    init PvP UI, functions

--- Assets/Idea02/Scripts/GameManager.cs
+++ Assets/Idea02/Scripts/GameManager.cs
@@ -6,15 +6,42 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
+    public Transform GamePlay;
     public Camera cam;
     public LineRenderer _line;
     //public Movement2 playermove;
     //public Hooks hook;
     public List<BulletSO> bulletSOs;
 
+    public List<GameObject> Maps;
+    public List<GameObject> Players;
 
     private void Awake()
     {
         instance = this;
+    }
+
+    public void InsPvPmaps()
+    {
+        int i = Random.Range(0, Maps.Count);
+        var t = Instantiate(Maps[i], GamePlay);
+    }
+    public void PlayerNumbOn(int number)
+    {
+        for(int i = 0; i < number; i++)
+        {
+            Players[i].SetActive(true);
+        }
+    }
+    public void PlayerOff()
+    {
+        foreach(var p in Players)
+        {
+            p.SetActive(false);
+        }
+    }
+    public void InsStorymaps()
+    {
+
     }
 }
--- Assets/Idea02/Scripts/UI/PvPUI.cs
+++ Assets/Idea02/Scripts/UI/PvPUI.cs
@@ -1,12 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PvPUI : MonoBehaviour
 {
+    [SerializeField] GameObject Player3Map;
+    [SerializeField] Button Play;
     public void PlayPvp()
     {
         UIController.instance.inGameUI.gameObject.SetActive(true);
         gameObject.SetActive(false);
+        Play.gameObject.SetActive(false);
+        Player3Map.SetActive(false);
     }
+    public void BackMain()
+    {
+        UIController.instance.mainUI.gameObject.SetActive(true);
+        gameObject.SetActive(false);
+        Play.gameObject.SetActive(false);
+        Player3Map.SetActive(false);
+    }
+
 }
